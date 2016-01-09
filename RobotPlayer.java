e126c6cc7b93fb23fb89c506911ecc3d30f7b3f7
Lucas Braune <lucasbraune@gmail.com>
2016-01-09 15:48:50 -0800

    Simple attack routine for soldiers
    Soldiers will now attack the weakest enemy within range instead of
    doing nothing

--- RobotPlayer.java
+++ RobotPlayer.java
@@ -1,14 +1,23 @@
 package artanis;
 
+import java.util.Random;
+
 import battlecode.common.*;
 
 public class RobotPlayer {
 
+	private static Random rnd;
+	
 	private static RobotController rc;
+	private static Team myTeam, enemyTeam; 
 	
 	public static void run(RobotController rcIn){
 		
 		rc = rcIn;
+		myTeam = rc.getTeam();
+		enemyTeam = myTeam.opponent();
+		
+		rnd = new Random(rc.getID());
 		
 		while(true){
 			try{
@@ -32,9 +41,41 @@ public class RobotPlayer {
 		}
 	}
 
-	private static void soldierCode() {
-		// TODO Auto-generated method stub
+	private static void soldierCode() throws GameActionException {
+		int myAttackRadius = rc.getType().attackRadiusSquared;
 		
+		if(rc.isWeaponReady()){
+			RobotInfo[] enemyRobots = rc.senseHostileRobots(rc.getLocation(), myAttackRadius);
+			if(enemyRobots.length>0){
+				rc.attackLocation( findWeakestEnemy(enemyRobots) );
+			}
+			
+		}
+			
+	}
+	
+	private static MapLocation findWeakestEnemy( RobotInfo[] enemyRobots ) {
+		int numberOfEnemies = enemyRobots.length;
+		
+		if( numberOfEnemies > 0 ){
+			int i, weakestIndex = 0;
+			double weakness = 0, largestWeakness;
+	
+			largestWeakness = enemyRobots[0].maxHealth - enemyRobots[0].health;
+	
+			for(i=1; i<numberOfEnemies; i++) {
+				weakness = enemyRobots[i].maxHealth - enemyRobots[i].health;
+				if ( weakness > largestWeakness ){
+					weakestIndex = i;
+					largestWeakness = weakness;
+				}
+			}
+			
+			return enemyRobots[weakestIndex].location;
+			
+		} else {
+			return null;
+		}
 	}
 
 	private static void scoutCode() {
@@ -57,8 +98,18 @@ public class RobotPlayer {
 		
 	}
 
-	private static void archonCode() {
-		// TODO Auto-generated method stub
-		
+	private static void archonCode() throws GameActionException {
+		if(rc.isCoreReady()){
+			Direction randomDir = randomDirection();
+			if(rc.canBuild(randomDir, RobotType.SOLDIER)){
+				rc.build(randomDir,RobotType.SOLDIER);
+			} 
+		}
 	}
+	
+
+	private static Direction randomDirection() {
+		return Direction.values()[(int)(rnd.nextDouble()*8)];
+	}
+	
 }
